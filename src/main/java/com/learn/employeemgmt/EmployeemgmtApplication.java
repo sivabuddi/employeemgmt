ac94591b76d3c93021973e2b76993da0d577002f
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2022-11-11 14:34:53 -0600

    Initial commit

---
+++ src/main/java/com/learn/employeemgmt/EmployeemgmtApplication.java
@@ -0,0 +1,14 @@
+package com.learn.employeemgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemgmtApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EmployeemgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/employeemgmt/EmployeemgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.employeemgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
